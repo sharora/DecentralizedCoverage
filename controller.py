added consensus term and graphing of parameter variance, the parameters seem to converge but I don't know why they are always going down and converging to the wrong numbers(by a lot), will need to search for errors in update code

controller.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial import KDTree
+from scipy.spatial import Delaunay
 from linearbasis import GaussianBasis
 
 
@@ -21,7 +22,7 @@ class Controller(object):
     that was used in representing the true sensing function, and that will be used for
     representing each robot's estimate of the sensing function
     '''
-    def __init__(self, qlis, phi, qcoor, res, mulis, sigmalis, amin, gamma):
+    def __init__(self, qlis, phi, qcoor, res, mulis, sigmalis, amin, gamma, incl_consensus=False, zeta=0):
         super().__init__()
         self._qlis = qlis
         self._numrobot = qlis.shape[0]
@@ -50,6 +51,10 @@ class Controller(object):
         #gamma is the learning rate
         self._gamma = gamma
 
+        #boolean telling us whether we include the consensus term
+        self._consensus = incl_consensus
+        self._zeta = zeta
+
         #we will use constant weighting function when computing Lambda and lambda
         #for now, TODO is to see how adding a function changes things
 
@@ -69,6 +74,11 @@ class Controller(object):
         #updating voronoi regions
         self._kdtree = KDTree(self._qlis)
 
+        #creating adjaceny matrix for consensus term
+        c_terms = None
+        if(self._consensus):
+            c_terms = self.consensus_terms()
+
         #Compute all integrals over voronoi regions
         self.computeVoronoiIntegrals()
         #update all parameters
@@ -76,6 +86,9 @@ class Controller(object):
             #equation 13, finding adots
             acurr_i = self._phihatlist[i].getparam()
             adot_i = -self._F[i] @ acurr_i - self._gamma * (self._Lambda[i] @ acurr_i - np.reshape(self._lambda[i], (self._basislen, 1)))
+
+            if(c_terms != None):
+                adot_i -= self._zeta * c_terms[i]
 
             #equation 14, projection step
             for j in range(self._basislen):
@@ -101,6 +114,32 @@ class Controller(object):
 
         #returning the current state
         return self._qlis
+
+    def consensus_terms(self):
+        #creating the Delaunay triangulation which is the dual graph of the voronoi partition
+        tri = Delaunay(self._qlis).simplices
+        c_terms = []
+
+        #storing all the parameters of each robot
+        a_curr = []
+        for i in range(self._numrobot):
+            a_curr.append(self._phihatlist[i].getparam())
+            c_terms.append(np.zeros((self._basislen, 1)))
+
+        #looping over each triangle and adding each edge to each consensus sum
+        for i in range(tri.shape[0]):
+            curr = tri[i]
+            v1 = curr[0]
+            v2 = curr[1]
+            v3 = curr[2]
+
+            #we are choosing the weighting in remark 5 because it simpler for now
+            #TODO try the weighting given by the edge length shared by the 2 voronoi regions
+            c_terms[v1] += (a_curr[v1] - a_curr[v2]) + (a_curr[v1] - a_curr[v3])
+            c_terms[v2] += (a_curr[v2] - a_curr[v1]) + (a_curr[v2] - a_curr[v3])
+            c_terms[v3] += (a_curr[v3] - a_curr[v1]) + (a_curr[v3] - a_curr[v2])
+
+        return c_terms
 
     def computeVoronoiIntegrals(self):
         '''

main.py
@@ -25,7 +25,7 @@ sigmalis = [
     [[0.5, 0], [0, 0.5]],
     [[0.5, 0], [0, 0.5]]
 ]
-truea = np.array([3.0, 8.0])
+truea = np.array([[3.0], [8.0]])
 amin = np.array([0.1, 0.1]) 
 truephi = GaussianBasis(mulis, sigmalis)
 truephi.updateparam(truea)
@@ -33,12 +33,14 @@ truephi.updateparam(truea)
 #defining the controller to drive robots to locally optimal configuration
 res = (8,8) #resolution tells us how many regions to divide each axis into
 gamma = 0.1 #learning rate
-c = Controller(qlis, truephi, qcoor, res, mulis, sigmalis, amin, gamma)
+c = Controller(qlis, truephi, qcoor, res, mulis, sigmalis, amin, gamma, True, 0.02)
 
 #lists for tracking distance between robot parameters and true parameters
 adislist = []
 for i in range(numrobot):
     adislist.append([])
+
+vargraph = []
 
 #main simulation loop
 graphcolors = np.random.rand(numrobot)
@@ -53,13 +55,28 @@ for i in range(numsteps):
     plt.draw()
     plt.pause(0.02)
 
+    varlist = []
     #adding parameter distances to list
     for j in range(numrobot):
         dist = np.linalg.norm(c._phihatlist[j].getparam() - truephi.getparam())
         adislist[j].append(dist)
+        varlist.append(c._phihatlist[j].getparam())
+    #computing and storing variance of parameters
+    varlist = np.array(varlist)
+    m = np.mean(varlist, axis=0)
+    var = 0
+    for j in range(numrobot):
+        var += np.transpose(c._phihatlist[j].getparam() - m) @ (c._phihatlist[j].getparam() - m)
+    var = var/numrobot
+    vargraph.append(var[0][0])
+
+plt.clf()
+
+#graphing variance of robot parameters with respect to time
+plt.plot(np.array(vargraph))
+plt.show()
 
 #graphing parameter distances with respect to time
-plt.clf()
 for i in range(numrobot):
     plt.plot(np.array(adislist[i]))
     plt.show()